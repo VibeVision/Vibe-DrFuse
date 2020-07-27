723c1e56ac9b60f8a53822f68e451e1324338773
VibeVision <VibeVisio80@proton.me>
2020-07-27 09:00:56 +0000

    drivers/rtc/rtc-twl.c: ensure all interrupts are disabled during probe

--- models/drfuse.py
+++ models/drfuse.py
@@ -17,4 +17,44 @@ class DrFuseModel(nn.Module):
                                         d_model=hidden_size, n_head=ehr_n_head,
                                         n_layers_feat=1, n_layers_shared=ehr_n_layers,
                                         n_layers_distinct=ehr_n_layers,
-                             
+                                        dropout=ehr_dropout)
+
+        resnet = resnet50()
+        self.cxr_model_feat = nn.Sequential(
+            resnet.conv1,
+            resnet.bn1,
+        )
+
+        resnet = resnet50()
+        self.cxr_model_shared = nn.Sequential(
+            resnet.layer1,
+            resnet.layer2,
+            resnet.layer3,
+            resnet.layer4,
+        )
+        self.cxr_model_shared.fc = nn.Linear(in_features=resnet.fc.in_features, out_features=hidden_size)
+
+        resnet = resnet50()
+        self.cxr_model_spec = nn.Sequential(
+            resnet.layer1,
+            resnet.layer2,
+            resnet.layer3,
+            resnet.layer4
+        )
+        self.cxr_model_spec.fc = nn.Linear(in_features=resnet.fc.in_features, out_features=hidden_size)
+
+        self.shared_project = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size*2),
+            nn.ReLU(),
+            nn.Linear(hidden_size*2, hidden_size),
+            nn.ReLU(),
+            nn.Linear(hidden_size, hidden_size)
+        )
+
+        self.ehr_model_linear = nn.Linear(in_features=hidden_size, out_features=num_classes)
+        self.cxr_model_linear = nn.Linear(in_features=hidden_size, out_features=num_classes)
+        self.fuse_model_shared = nn.Linear(in_features=hidden_size, out_features=num_classes)
+
+        self.domain_classifier = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size//2),
+            
